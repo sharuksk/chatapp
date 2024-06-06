b1b07230f02d3ea6d612f127fe1d124a9dc45a9f
sharuksk <sharukajmal2@gmail.com>
2024-06-06 21:00:46 +0300

    layout configured

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import MainContainer from './Components/MainContainer';
 
 function App() {
   return (
     <div className="App">
+      <MainContainer/>
     </div>
   );
 }
---
+++ src/Components/Sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './myStyles.css'
+
+function Sidebar() {
+  return (
+    <div className='sidebar-container'>
+      Sidebar
+    </div>
+  )
+}
+
+export default Sidebar
