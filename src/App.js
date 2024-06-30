05e9d253c34785dd1d2c5f99c5dd50e7549a24ed
sharuksk <sharukajmal2@gmail.com>
2024-06-30 21:03:39 +0300

    revert

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import MainContainer from './Components/MainContainer';
 import Login from './Components/Login';
 import CreateGroups from './Components/CreateGroups';
 
-//push
 function App() {
   return (
     <div className="App">
